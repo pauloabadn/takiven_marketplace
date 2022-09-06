c7493da3087f9c32b8bd085d539ff387c0328c98
Cesar Abad <cesarnavarroabad@gmail.com>
2022-09-06 18:55:49 -0500

    Se agregaron nuevos cambios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const burgerOpen = document.querySelector(".burgerOpen"),
-      menu = document.querySelector(".menu")
-    
+      burgerClose = document.querySelector(".burgerClose"),
+      menu = document.querySelector(".menu");
+
 burgerOpen.addEventListener("click" , () =>{
-    menu.classList.add(".active");
+    menu.classList.add("active");
+});
+
+burgerClose.addEventListener("click" , () =>{
+    menu.classList.remove("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("burgerOpen") && !clickedElm.classList.contains("menu")){
+        menu.classList.remove("active");
+    }
 });
