121d51f600d94240f77dcc9d01c2b944673cc222
Cesar Abad <cesarnavarroabad@gmail.com>
2022-09-16 18:53:56 -0500

    Se agregan nuevas Funcione JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,59 @@
-const burgerOpen = document.querySelector(".burgerOpen"),
-      burgerClose = document.querySelector(".burgerClose"),
-      menu = document.querySelector(".menu");
+//const burgerOpen = document.querySelector(".burgerOpen"),
+//      burgerClose = document.querySelector(".burgerClose"),
+//      menu = document.querySelector(".menu");
 
-burgerOpen.addEventListener("click" , () =>{
-    menu.classList.add("active");
+//burgerOpen.addEventListener("click" , () =>{
+//    menu.classList.add("active");
+//});
+
+//burgerClose.addEventListener("click" , () =>{
+//    menu.classList.remove("active");
+//});
+
+//body.addEventListener("click" , e =>{
+//    let clickedElm = e.target;
+
+//    if(!clickedElm.classList.contains("burgerOpen") && clickedElm.classList.contains("menu")){
+//        menu.classList.remove("active");
+//    }
+//});
+
+
+const contenedor = document.getElementById('contenido');
+
+sneakers.forEach( producto => {
+
+    const div = document.createElement('div');
+    div.classList.add('item');
+
+    div.innerHTML = `
+                        <div class="cont">
+                            <div class="img"><img src="${producto.img}" alt=""></div>
+                            <h3><span>${producto.marca}</span> <br> ${producto.nombre}</h3>
+                            <div class="cont-text">
+                                <div class="precio"><p>S/${producto.precio}</p></div>
+                                <div class="oferta"><p>S/${producto.oferta}</p></div>
+                            </div>
+                            <div class="agregar"><p>Agregar</p></div>
+    `;
+    contenedor.appendChild(div);
 });
 
-burgerClose.addEventListener("click" , () =>{
-    menu.classList.remove("active");
-});
+const contgamer = document.getElementById('contgamer');
+gamer.forEach( producto => {
 
-body.addEventListener("click" , e =>{
-    let clickedElm = e.target;
+    const div = document.createElement('div');
+    div.classList.add('item');
 
-    if(!clickedElm.classList.contains("burgerOpen") && !clickedElm.classList.contains("menu")){
-        menu.classList.remove("active");
-    }
+    div.innerHTML = `
+                        <div class="cont">
+                            <div class="img"><img src="${producto.img}" alt=""></div>
+                            <h3><span>${producto.marca}</span> <br> ${producto.nombre}</h3>
+                            <div class="cont-text">
+                                <div class="precio"><p>S/${producto.precio}</p></div>
+                                <div class="oferta"><p>S/${producto.oferta}</p></div>
+                            </div>
+                            <div class="agregar"><p>Agregar</p></div>
+    `;
+    contgamer.appendChild(div);
 });
---
+++ js/stock.js
@@ -0,0 +1,148 @@
+const sneakers =
+[
+    {
+        id: 1,
+        marca: "Nike",
+        nombre: "Buzo 1",
+        tipo: "buzo",
+        desc: "Un buzo que re va con vos",
+        precio: 1200.99,
+        oferta: 599.99,
+        talle: "L",
+        img: '/public/images/buzo.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        marca: "Adidas",
+        nombre: "Remera 1",
+        tipo: "remera",
+        desc: "Una remera que re va con vos",
+        precio: 500.99,
+        oferta: 250.99,
+        talle: "L",
+        img: '/public/images/remera.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        marca: "Rebook",
+        nombre: "Camisa 1",
+        tipo: "camisa",
+        desc: "Una camisa que re va con vos",
+        precio: 900.99,
+        oferta: 490.99,
+        talle: "S",
+        img: '/public/images/camisa.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        marca: "Diadora",
+        nombre: "Pantalon 1",
+        tipo: "pantalon",
+        desc: "Una pantalon que re va con vos",
+        precio: 1600.99,
+        oferta: 840.99,
+        talle: "L",
+        img: '/public/images/pantalon.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        marca: "Umbro",
+        nombre: "Buzo 2",
+        tipo: "buzo",
+        desc: "Un buzo que re va con vos",
+        precio: 1200,
+        oferta: 790.99,
+        talle: "L",
+        img: '/public/images/buzo.jpg',
+        cantidad:1
+    },
+    
+    {
+        id: 6,
+        marca: "JosePau",
+        nombre: "Buzo 2",
+        tipo: "buzo",
+        desc: "Un buzo que re va con vos",
+        precio: 1200,
+        oferta: 790.99,
+        talle: "L",
+        img: '/public/images/buzo.jpg',
+        cantidad:1
+    }
+];
+
+const gamer =
+[
+    {
+        id: 1,
+        marca: "Logitech",
+        nombre: "Buzo 1",
+        tipo: "buzo",
+        desc: "Un buzo que re va con vos",
+        precio: 1200.99,
+        oferta: 599.99,
+        talle: "L",
+        img: '/public/images/buzo.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        marca: "Samsung",
+        nombre: "Remera 1",
+        tipo: "remera",
+        desc: "Una remera que re va con vos",
+        precio: 500.99,
+        oferta: 250.99,
+        talle: "L",
+        img: '/public/images/remera.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        marca: "Rebook",
+        nombre: "Camisa 1",
+        tipo: "camisa",
+        desc: "Una camisa que re va con vos",
+        precio: 900.99,
+        oferta: 490.99,
+        talle: "S",
+        img: '/public/images/camisa.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        marca: "Diadora",
+        nombre: "Pantalon 1",
+        tipo: "pantalon",
+        desc: "Una pantalon que re va con vos",
+        precio: 1600.99,
+        oferta: 840.99,
+        talle: "L",
+        img: '/public/images/pantalon.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        marca: "Umbro",
+        nombre: "Buzo 2",
+        tipo: "buzo",
+        desc: "Un buzo que re va con vos",
+        precio: 1200,
+        oferta: 790.99,
+        talle: "L",
+        img: '/public/images/buzo.jpg',
+        cantidad:1
+    }
+];
